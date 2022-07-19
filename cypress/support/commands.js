670ae3b969bb04f4293d69ba18196d8395a3af41
unknown <tomaszpeszek2000@gmail.com>
2022-07-19 11:56:11 +0200

    testy-logowaniafix

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,33 +1,7 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
 function generateTestSelector(value) {
     return `[data-test=${value}]`;
 }
 
 Cypress.Commands.add("getSelector", (value) => {
     return cy.get(generateTestSelector(value))
-});
+}); 
