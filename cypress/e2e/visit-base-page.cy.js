4f8235c7fd6cbb6e248db3f32c9206540f8c6e01
tomcopl <109338512+tomcopl@users.noreply.github.com>
2022-07-19 13:12:57 +0200

    testy-logowania (#1)
    * testy-logowania
    
    * testy-logowaniafix
    
    * cleaning

--- cypress/e2e/visit-base-page.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Sample describe', () => {
-
-  it('Sample name f.e. Should open home page and check url', () => {
-    cy.visit('/').url().should('eq', 'https://www.saucedemo.com/')
-  })
-})
