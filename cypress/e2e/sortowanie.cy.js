af0dfee5168318bda40e448112015c75b6b06084
unknown <tomaszpeszek2000@gmail.com>
2022-07-19 10:14:37 +0200

    sortowanie

---
+++ cypress/e2e/sortowanie.cy.js
@@ -0,0 +1,35 @@
+describe('Sample describe', () => {
+    beforeEach('', ()=>{
+        cy.visit('/');
+    });
+    it('1.2.2.1. Sortowanie produktów po nazwie asc', () => {
+        cy.url().should('eq', 'https://www.saucedemo.com/')
+    
+        cy.getSelector("username").type("standard_user");
+        cy.getSelector("password").type("secret_sauce")
+        cy.getSelector("login-button").click();
+
+        cy.get(".inventory_item_name").then(($elements) => {
+            console.log([...$elements].map((el) => el.innerText))
+            const strings = [...$elements].map((el) => el.innerText);
+            expect(strings).to.deep.equal([...strings].sort());
+          });
+      })
+
+    it('1.2.2.2. Sortowanie produktów po nazwie desc', () => {
+        cy.url().should('eq', 'https://www.saucedemo.com/')
+    
+        cy.getSelector("username").type("standard_user");
+        cy.getSelector("password").type("secret_sauce")
+        cy.getSelector("login-button").click();
+        cy.get('[data-test="product_sort_container"]').select('Name (Z to A)')
+
+        cy.get(".inventory_item_name").then(($elements) => {
+            console.log([...$elements].map((el) => el.innerText))
+            const strings = [...$elements].map((el) => el.innerText);
+            expect(strings).to.deep.equal([...strings].sort().reverse());
+          });
+      })
+    
+
+})
